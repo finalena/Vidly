d03a5b128c3e5e6966bf1faaf93646c59f079a92
Lena <islenaliny@gmail.com>
2021-04-27 20:35:07 +0800

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Models.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random(string MovieId)
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customer = new List<Customer>
+            {
+                new Customer{Name = "Customer1" },
+                new Customer{Name = "Customer2" }
+            };
+
+            var viewModel = new RandomCustomerViewModel()
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(string MovieId)
+        {
+            return Content("MovieId = " + MovieId);
+        }
+
+        public ActionResult Index(int? pageIndex , string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("PageIndex={0}, sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Vidly/Models/ViewModels/RandomCustomerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models.ViewModels
+{
+    public class RandomCustomerViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
